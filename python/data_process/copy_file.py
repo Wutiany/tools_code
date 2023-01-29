618b79ed350ea314c659309b7d01f4707c27f6a4
Wutiany <1392508773@qq.com>
2023-01-29 11:55:06 +0800

    move_file

--- python/data_process/copy_file.py
+++ python/data_process/copy_file.py
@@ -2,19 +2,20 @@ import os
 import shutil
 
 
-def move_file(dir_path, move_path, name_list):
-    name_list = os.listdir(name_list)
+def move_file(dir_path, move_path, name_path):
+    name_list = os.listdir(name_path)
     file_type = os.listdir(dir_path)[0].split('.')[-1]
     # 改同名文件的文件类型
-    name_list = [name.split('.')[0] + file_type for name in name_list]
-
+    name_list = [name.split('.')[0] + '.' +  file_type for name in name_list]
+    for name in name_list:
+        shutil.copy(os.path.join(dir_path, name), os.path.join(move_path, name))
 
 
 
 
 
 if __name__ == "__main__":
-    dir_path = r""
-    move_path = r""
-    name_list = []
-    move_file(dir_path, move_path, name_list)
+    dir_path = r"E:\data\zip\east_china_sea_20_34_117_132\2014"
+    move_path = r"E:\data\eddy_dataset_augment\eddy_img"
+    name_path = r"E:\data\eddy_dataset_augment\eddy_json"
+    move_file(dir_path, move_path, name_path)
